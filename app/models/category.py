928c88b86c0676debac17630dcdc329515bbd056
Joshua <joshua.hulford@gmail.com>
2023-06-13 15:40:22 -0400

    complete category model

---
+++ app/models/category.py
@@ -0,0 +1,19 @@
+from .db import db, environment, SCHEMA, add_prefix_for_prod
+
+class Category(db.Model):
+    __tablename__ = "categories"
+
+    if environment == "production":
+        __table_args__ = {'schema': SCHEMA}
+
+
+    id = db.Column(db.Integer, primary_key=True)
+    type = db.Column(db.String(50),nullable=False,unique=True)
+
+    trail = db.relationship("Trail",back_populates="category")
+
+    def to_dict(self):
+        return{
+            "id":self.id,
+            "type":self.type,
+            }
