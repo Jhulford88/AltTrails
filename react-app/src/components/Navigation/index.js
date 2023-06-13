b2296904dbd12e5ca379ead48c252aa82f220daa
Joshua <joshua.hulford@gmail.com>
2023-06-13 08:22:45 -0400

    checkout dev from main

--- react-app/src/components/Navigation/index.js
+++ react-app/src/components/Navigation/index.js
@@ -3,7 +3,7 @@ import { NavLink } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import ProfileButton from './ProfileButton';
 import './Navigation.css';
-
+//
 function Navigation({ isLoaded }){
 	const sessionUser = useSelector(state => state.session.user);
 
