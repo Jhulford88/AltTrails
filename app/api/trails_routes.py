49adb06d9b3ad0dd7559b13c3ce4e856450dfcdd
Joshua <joshua.hulford@gmail.com>
2023-06-14 18:43:03 -0400

    create trails_routes.py and register BP

---
+++ app/api/trails_routes.py
@@ -0,0 +1,15 @@
+from flask import Blueprint, jsonify
+from flask_login import login_required
+from app.models import User
+
+user_routes = Blueprint('trails', __name__)
+
+
+# @user_routes.route('/')
+# @login_required
+# def users():
+#     """
+#     Query for all users and returns them in a list of user dictionaries
+#     """
+#     users = User.query.all()
+#     return {'users': [user.to_dict() for user in users]}
