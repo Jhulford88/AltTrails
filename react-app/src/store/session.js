32f5e1a06a3d36c493224c2519dfe4236f6ab72e
Joshua <joshua.hulford@gmail.com>
2023-06-23 13:12:58 -0400

    cleanup session reducer

--- react-app/src/store/session.js
+++ react-app/src/store/session.js
@@ -1,127 +1,114 @@
-// constants
+// ------------ TYPE VARIABLES ------------
 const SET_USER = "session/SET_USER";
 const REMOVE_USER = "session/REMOVE_USER";
-// const CREATE_FAVORITE = "session/CREATE_FAVORITE"
 
+// ---------- ACTION CREATORS ----------
 const setUser = (user) => ({
-	type: SET_USER,
-	payload: user,
+  type: SET_USER,
+  payload: user,
 });
 
 const removeUser = () => ({
-	type: REMOVE_USER,
+  type: REMOVE_USER,
 });
 
-// const createFavorite = (user) => {
-//   return {
-//     type: CREATE_FAVORITE,
-// 	payload: user
-//   }
-// }
-
-const initialState = { user: null };
-
+// ---------- THUNKS ----------
 export const authenticate = () => async (dispatch) => {
-	const response = await fetch("/api/auth/", {
-		headers: {
-			"Content-Type": "application/json",
-		},
-	});
-	if (response.ok) {
-		const data = await response.json();
-		if (data.errors) {
-			return;
-		}
+  const response = await fetch("/api/auth/", {
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+  if (response.ok) {
+    const data = await response.json();
+    if (data.errors) {
+      return;
+    }
 
-		dispatch(setUser(data));
-	}
+    dispatch(setUser(data));
+  }
 };
 
 export const login = (email, password) => async (dispatch) => {
-	const response = await fetch("/api/auth/login", {
-		method: "POST",
-		headers: {
-			"Content-Type": "application/json",
-		},
-		body: JSON.stringify({
-			email,
-			password,
-		}),
-	});
+  const response = await fetch("/api/auth/login", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      email,
+      password,
+    }),
+  });
 
-	if (response.ok) {
-		const data = await response.json();
-		dispatch(setUser(data));
-		return null;
-	} else if (response.status < 500) {
-		const data = await response.json();
-		if (data.errors) {
-			return data.errors;
-		}
-	} else {
-		return ["An error occurred. Please try again."];
-	}
+  if (response.ok) {
+    const data = await response.json();
+    dispatch(setUser(data));
+    return null;
+  } else if (response.status < 500) {
+    const data = await response.json();
+    if (data.errors) {
+      return data.errors;
+    }
+  } else {
+    return ["An error occurred. Please try again."];
+  }
 };
 
 export const logout = () => async (dispatch) => {
-	const response = await fetch("/api/auth/logout", {
-		headers: {
-			"Content-Type": "application/json",
-		},
-	});
+  const response = await fetch("/api/auth/logout", {
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
 
-	if (response.ok) {
-		dispatch(removeUser());
-	}
+  if (response.ok) {
+    dispatch(removeUser());
+  }
 };
 
 export const signUp = (formData) => async (dispatch) => {
-	const response = await fetch("/api/auth/signup", {
-		method: "POST",
-		// headers: {
-		// 	"Content-Type": "application/json",
-		// },
-		body: formData
-	});
+  const response = await fetch("/api/auth/signup", {
+    method: "POST",
+    body: formData,
+  });
 
-	if (response.ok) {
-		const data = await response.json();
-		dispatch(setUser(data));
-		return null;
-	} else if (response.status < 500) {
-		const data = await response.json();
-		if (data.errors) {
-			return data.errors;
-		}
-	} else {
-		return ["An error occurred. Please try again."];
-	}
+  if (response.ok) {
+    const data = await response.json();
+    dispatch(setUser(data));
+    return null;
+  } else if (response.status < 500) {
+    const data = await response.json();
+    if (data.errors) {
+      return data.errors;
+    }
+  } else {
+    return ["An error occurred. Please try again."];
+  }
 };
 
-//moving over the createFavorite thunk
-
 export const createFavoriteThunk = (trailId) => async (dispatch) => {
-	trailId = parseInt(trailId)
-	const res = await fetch(`/api/trails/favorites/${trailId}`, {
-	  method: "POST"
-	})
-	if (res.ok) {
-	  let response = await res.json()
-	  dispatch(setUser(response))
-	}
+  trailId = parseInt(trailId);
+  const res = await fetch(`/api/trails/favorites/${trailId}`, {
+    method: "POST",
+  });
+  if (res.ok) {
+    let response = await res.json();
+    dispatch(setUser(response));
   }
+};
 
+// --------- INITIAL STATE -------------
+const initialState = { user: null };
+
+// ---------- REDUCER ----------
 export default function reducer(state = initialState, action) {
-	switch (action.type) {
-		case SET_USER:
-			return { user: action.payload };
-		case REMOVE_USER:
-			return { user: null };
-		// case CREATE_FAVORITE:
-		// 	//???????????????????????????
-		// 	let newState = { ...state }
-		// 	return newState.favorites.push(action.favorite)
-		default:
-			return state;
-	}
+  switch (action.type) {
+    case SET_USER:
+      return { user: action.payload };
+    case REMOVE_USER:
+      return { user: null };
+    default:
+      return state;
+  }
 }
