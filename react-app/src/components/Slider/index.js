a85f4e77e46457a744e364d0d277a2db10ceff18
Joshua <joshua.hulford@gmail.com>
2023-06-23 12:53:55 -0400

    cleanup slider and css

--- react-app/src/components/Slider/index.js
+++ react-app/src/components/Slider/index.js
@@ -1,38 +1,40 @@
-import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
-import { Carousel } from 'react-responsive-carousel';
-import image1 from "../../assets/slider-2.avif"
-import image2 from "../../assets/slider-3.avif"
-import image3 from "../../assets/slider-5.avif"
-import "./slider.css"
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from "react-responsive-carousel";
 import { useSelector } from "react-redux";
-
-
-
+import image1 from "../../assets/slider-2.avif";
+import image2 from "../../assets/slider-3.avif";
+import image3 from "../../assets/slider-5.avif";
+import "./slider.css";
 
 const Slider = () => {
+  //State
+  const user = useSelector((state) => state.session.user);
 
-    const user = useSelector(state => state.session.user)
-
-    return (
+  return (
+    <div>
+      <p className="test">
+        Let's find a new trail{user ? ", " + user.firstName : null}
+      </p>
+      <Carousel
+        className="carousel"
+        showThumbs={false}
+        autoPlay={true}
+        infiniteLoop={true}
+        showStatus={false}
+        showArrows={false}
+      >
         <div>
-            <p className="test">Let's find a new trail{user ? ", " + user.firstName : null}</p>
-            <Carousel className="carousel" showThumbs={false} autoPlay={true} infiniteLoop={true} showStatus={false} showArrows={false}>
-                <div>
-                    <img className="car-img" src={image1} />
-
-                </div>
-                <div>
-                    <img className="car-img" src={image2} />
-
-                </div>
-                <div>
-                    <img className="car-img" src={image3} />
-
-                </div>
-            </Carousel>
+          <img className="car-img" src={image1} />
         </div>
-    );
+        <div>
+          <img className="car-img" src={image2} />
+        </div>
+        <div>
+          <img className="car-img" src={image3} />
+        </div>
+      </Carousel>
+    </div>
+  );
+};
 
-}
-
-export default Slider
+export default Slider;
