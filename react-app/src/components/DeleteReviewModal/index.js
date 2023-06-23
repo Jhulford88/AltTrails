e4f75650d494298bc1b7d18f8bd8e4c3df617142
Joshua <joshua.hulford@gmail.com>
2023-06-23 12:18:03 -0400

    cleanup deleteReviewModal

--- react-app/src/components/DeleteReviewModal/index.js
+++ react-app/src/components/DeleteReviewModal/index.js
@@ -1,39 +1,38 @@
 import { useDispatch } from "react-redux";
 import { useModal } from "../../context/Modal";
-import { useHistory } from 'react-router-dom';
-import { deleteTrailThunk } from "../../store/trails";
 import { getCurrentTrailsThunk } from "../../store/trails";
-import './deleteReviewModal.css'
 import { deleteReviewThunk } from "../../store/trails";
+import "./deleteReviewModal.css";
 
+const DeleteReviewModal = ({ reviewId }) => {
+  //Initialize things
+  const { closeModal } = useModal();
+  const dispatch = useDispatch();
 
-const DeleteReviewModal = ({reviewId}) => {
+  //Dispatch thunk to delete review
+  const deleteReview = (e) => {
+    dispatch(deleteReviewThunk(reviewId))
+      .then(dispatch(getCurrentTrailsThunk()))
+      .then(closeModal);
+  };
 
-    //initialize things
-    const { closeModal } = useModal()
-    const dispatch = useDispatch()
-    const history = useHistory()
-
-
-    const deleteReview = (e) => {
-        dispatch(deleteReviewThunk(reviewId))
-          .then(dispatch(getCurrentTrailsThunk()))
-          .then(closeModal)
-      }
-
-
-      return (
-        <div className="delete-modal-container">
-          <div className="delete-modal-contents">
-            <h2 className="delete-modal-header">Are you sure you want to delete your review?</h2>
-            <div className="delete-button-container">
-            <button className="delete-yes-button" onClick={deleteReview}>Yes</button>
-            <button className="delete-cancel-button" onClick={closeModal}>Cancel</button>
-            </div>
-          </div>
+  return (
+    <div className="delete-modal-container">
+      <div className="delete-modal-contents">
+        <h2 className="delete-modal-header">
+          Are you sure you want to delete your review?
+        </h2>
+        <div className="delete-button-container">
+          <button className="delete-yes-button" onClick={deleteReview}>
+            Yes
+          </button>
+          <button className="delete-cancel-button" onClick={closeModal}>
+            Cancel
+          </button>
         </div>
-      )
+      </div>
+    </div>
+  );
+};
 
-}
-
-export default DeleteReviewModal
+export default DeleteReviewModal;
