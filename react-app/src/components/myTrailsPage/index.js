dc4f1ce6eaddd2a509d6b377da1427f82555b027
Joshua <joshua.hulford@gmail.com>
2023-06-23 12:42:25 -0400

    clean up myTrailsPage and css

--- react-app/src/components/myTrailsPage/index.js
+++ react-app/src/components/myTrailsPage/index.js
@@ -1,121 +1,151 @@
-import React, { useState, useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { useHistory } from 'react-router-dom';
-import { getCurrentTrailsThunk } from '../../store/trails';
-import { deleteFavoriteThunk } from '../../store/trails';
-import { authenticate } from '../../store/session';
-import OpenModalButton from '../OpenModalButton';
+import React, { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useHistory } from "react-router-dom";
+import { getCurrentTrailsThunk } from "../../store/trails";
+import { deleteFavoriteThunk } from "../../store/trails";
+import { authenticate } from "../../store/session";
+import OpenModalButton from "../OpenModalButton";
 import DeleteTrailModal from "../DeleteTrailModal";
-import image from "../../assets/slider-5.avif"
-import "./myTrailsPage.css"
-
+import image from "../../assets/slider-5.avif";
+import "./myTrailsPage.css";
 
 const MyTrailsPage = () => {
+  //Initialize things
+  const dispatch = useDispatch();
+  const history = useHistory();
 
-    //initialize things
-    const dispatch = useDispatch()
-    const history = useHistory()
+  //useSelectors
+  const user = useSelector((state) => state.session.user);
+  const favorites = user?.favorites;
+  const trails = useSelector((state) => state.trails.userTrails);
 
+  //State (to force reload of state after delete)
+  const [deleted, setDeleted] = useState(false);
 
-    //useSelectors
-    const user = useSelector(state => state.session.user)
-    const favorites = user?.favorites
-    const trails = useSelector(state => state.trails.userTrails)
+  //Dispatch thunk to get users trails
+  useEffect(() => {
+    dispatch(getCurrentTrailsThunk());
+    setDeleted(false);
+    dispatch(authenticate());
+  }, [dispatch, deleted]);
 
-    //state (to force reload of state after delete)
-    const [deleted, setDeleted] = useState(false)
+  //Delete favorite from DB and update store
+  const removeFavorite = (trailId) => {
+    dispatch(deleteFavoriteThunk(trailId));
+    dispatch(authenticate());
+    setDeleted(true);
+  };
 
-    useEffect(() => {
-        dispatch(getCurrentTrailsThunk())
-        setDeleted(false)
-        dispatch(authenticate())
-    }, [dispatch, deleted])
+  //Number of reviews by user
+  const reviewsWritten = () => {
+    let sum = 0;
+    Object.values(trails).forEach((trail) => {
+      sum += trail.reviews.length;
+    });
+    return sum;
+  };
 
-    //Delete favorite from DB and update store
-    const removeFavorite = (trailId) => {
-        dispatch(deleteFavoriteThunk(trailId))
-        dispatch(authenticate())
-        setDeleted(true)
-    }
-
-    //Number of reviews by user
-    const reviewsWritten = () => {
-        let sum = 0
-        Object.values(trails).forEach(trail => {
-            sum += trail.reviews.length
-        })
-        return sum
-    }
-    console.log('review written.........',reviewsWritten())
-
-
-    const manageCards = Object.values(trails)?.map(trail => {
-        return (
-            <div className="manage-trail-card">
-                <div key={trail.id}  onClick={(e) => {
-                    history.push(`/trails/${trail.id}`)
-                    }}>
-                    <div className="manage-trail-card-img-container">
-                        <img className="manage-trail-card-img" alt="Trail Image" src={trail?.coverPhoto}></img>
-                    </div>
-                    <div className="manage-trail-name">{trail.trailName}</div>
-                    <div className="manage-park-name">{trail.park}</div>
-                    <div className="manage-park-stats">Length: {trail.length}mi · Est. {Math.floor(trail.length*17)}min</div>
-                </div>
-                    <div className='manage-button-container'>
-                        <button  onClick={() => history.push(`/trails/${trail.id}/update`)} >Update</button>
-                        &nbsp;&nbsp;<OpenModalButton
-                            className="trail-delete-button"
-                            buttonText={"Delete"}
-                            modalComponent={<DeleteTrailModal trailId={trail.id} setDeleted={setDeleted} />}
-                        />
-                    </div>
-            </div>
-        )
-      })
-
-
-      const favoriteCards = favorites?.map(trail => {
-        return (
-            <div className="manage-trail-card">
-                <div key={trail.id}  onClick={(e) => {
-                    history.push(`/trails/${trail.id}`)
-                    }}>
-                    <div className="manage-trail-card-img-container">
-                        <img className="manage-trail-card-img" alt="Trail Image" src={trail?.coverPhoto}></img>
-                    </div>
-                    <div className="manage-trail-name">{trail.trailName}</div>
-                    <div className="manage-park-name">{trail.park}</div>
-                    <div className="manage-park-stats">Length: {trail.length}mi · Est. {Math.floor(trail.length*17)}min</div>
-                </div>
-                    <div>
-                        <button onClick={() => {removeFavorite(trail.id)}}>Remove Favorite</button>
-                    </div>
-            </div>
-        )
-      })
-
-
-      if (!user) history.push('/')
-
+  //Build the current users trail cards
+  const manageCards = Object.values(trails)?.map((trail) => {
     return (
-        <div>
-            <div className="banner-img-container">
-                <img className='banner-img' src={image}></img>
-                <h1 className='banner-text'>Lets do this...</h1>
-            </div>
-            <div className='user-stats'>{user.favorites.length ? user.favorites.length : 0} Favorites! &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; {Object.values(trails).length ? Object.values(trails).length : 0} Trails Created! &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; {reviewsWritten()} Reviews Written!</div>
-            <h1 className='section-headers-top'>My Trails</h1>
-            <div id='manage-cards-top' className="manage-trail-cards-container">
-                {manageCards}
-            </div>
-            <h1 className='section-headers-bottom'>My Favorites</h1>
-            <div id='manage-cards-bottom' className="manage-trail-cards-container">
-                {favoriteCards}
-            </div>
-
+      <div className="manage-trail-card">
+        <div
+          key={trail.id}
+          onClick={(e) => {
+            history.push(`/trails/${trail.id}`);
+          }}
+        >
+          <div className="manage-trail-card-img-container">
+            <img
+              className="manage-trail-card-img"
+              alt="Trail Image"
+              src={trail?.coverPhoto}
+            ></img>
+          </div>
+          <div className="manage-trail-name">{trail.trailName}</div>
+          <div className="manage-park-name">{trail.park}</div>
+          <div className="manage-park-stats">
+            Length: {trail.length}mi · Est. {Math.floor(trail.length * 17)}min
+          </div>
         </div>
-    )
-}
+        <div className="manage-button-container">
+          <button onClick={() => history.push(`/trails/${trail.id}/update`)}>
+            Update
+          </button>
+          &nbsp;&nbsp;
+          <OpenModalButton
+            className="trail-delete-button"
+            buttonText={"Delete"}
+            modalComponent={
+              <DeleteTrailModal trailId={trail.id} setDeleted={setDeleted} />
+            }
+          />
+        </div>
+      </div>
+    );
+  });
 
-export default MyTrailsPage
+  //Build the users favorites trail cards
+  const favoriteCards = favorites?.map((trail) => {
+    return (
+      <div className="manage-trail-card">
+        <div
+          key={trail.id}
+          onClick={(e) => {
+            history.push(`/trails/${trail.id}`);
+          }}
+        >
+          <div className="manage-trail-card-img-container">
+            <img
+              className="manage-trail-card-img"
+              alt="Trail Image"
+              src={trail?.coverPhoto}
+            ></img>
+          </div>
+          <div className="manage-trail-name">{trail.trailName}</div>
+          <div className="manage-park-name">{trail.park}</div>
+          <div className="manage-park-stats">
+            Length: {trail.length}mi · Est. {Math.floor(trail.length * 17)}min
+          </div>
+        </div>
+        <div>
+          <button
+            onClick={() => {
+              removeFavorite(trail.id);
+            }}
+          >
+            Remove Favorite
+          </button>
+        </div>
+      </div>
+    );
+  });
+
+  if (!user) history.push("/");
+
+  return (
+    <div>
+      <div className="banner-img-container">
+        <img className="banner-img" src={image}></img>
+        <h1 className="banner-text">Lets do this...</h1>
+      </div>
+      <div className="user-stats">
+        {user.favorites.length ? user.favorites.length : 0} Favorites!
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{" "}
+        {Object.values(trails).length ? Object.values(trails).length : 0} Trails
+        Created! &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; {reviewsWritten()} Reviews
+        Written!
+      </div>
+      <h1 className="section-headers-top">My Trails</h1>
+      <div id="manage-cards-top" className="manage-trail-cards-container">
+        {manageCards}
+      </div>
+      <h1 className="section-headers-bottom">My Favorites</h1>
+      <div id="manage-cards-bottom" className="manage-trail-cards-container">
+        {favoriteCards}
+      </div>
+    </div>
+  );
+};
+
+export default MyTrailsPage;
