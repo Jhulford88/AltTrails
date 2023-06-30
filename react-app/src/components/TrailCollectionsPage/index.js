7330c09c699c0c3f051f2b06f402b29ec538c8de
Joshua <joshua.hulford@gmail.com>
2023-06-30 11:59:21 -0400

    cards rendering properly for now

--- react-app/src/components/TrailCollectionsPage/index.js
+++ react-app/src/components/TrailCollectionsPage/index.js
@@ -10,11 +10,38 @@ const CollectionsPage = () => {
   const dispatch = useDispatch();
 
   //useSelectors
+  const collections = useSelector((state) => state.collections.allCollections);
 
   //Dispatch Thunks
   useEffect(() => {
     dispatch(getAllCollectionsThunk());
   }, [dispatch]);
+
+  //Build collection cards to display
+  const cards = Object.values(collections)?.map((collection) => {
+    return (
+      <div
+        key={collection.id}
+        className="trail-card"
+        // onClick={(e) => {
+        //   history.push(`/trails/${trail.id}`);
+        // }}
+      >
+        <div className="trail-card-img-container">
+          <img
+            className="landing-trail-card-img"
+            alt="Trail Image"
+            src={collection?.trails[0].coverPhoto}
+          ></img>
+        </div>
+        <div className="trail-name">{collection.name}</div>
+        {/* <div className="park-name">{trail.park}</div> */}
+        <div>
+          {/* Length: {trail.length}mi · Est. {Math.floor(trail.length * 17)}min */}
+        </div>
+      </div>
+    );
+  });
 
   return (
     <div className="collections-page-parent-container">
@@ -22,6 +49,7 @@ const CollectionsPage = () => {
         <img className="collections-banner-img" src={image}></img>
         <h1 className="collections-banner-text">Collections</h1>
       </div>
+      <div className="trail-cards-container">{cards}</div>
     </div>
   );
 };
